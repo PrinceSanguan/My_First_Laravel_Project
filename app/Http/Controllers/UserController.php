ca9a28b1c205d13b8ef2b7e0af91aefc3dc03bd4
Prince Sanguan <princesanguan44@gmail.com>
2023-12-27 21:02:50 +0800

    Balik loob lang ulit sa laravel hirap aralin c#

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,19 +8,8 @@ use App\Models\User;
 class UserController extends Controller
 {
     public function index($id = null) {
-        $arr = [];
-
-        if ($id) {
-            // Select a specific user if $id is provided
-            $data = User::where('id', $id)->get();
-        } else {
-            // Select all users if $id is null
-            $data = User::all();
-        }
-
-        $arr['data'] = $data;
-
-        return view('user', $arr);
+  
+        return view('user');
     }
 }
 
